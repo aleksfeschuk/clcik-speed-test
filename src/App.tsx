5ff2709dd7d89cade48d9f51f936fcaa3931affb
Aleksandr Feshchuk <aleksfeschuk@gmail.com>
2025-07-13 15:50:55 -0600

    finish code a small project

--- src/App.tsx
+++ src/App.tsx
@@ -29,17 +29,75 @@ const App: React.FC = () => {
     }, 1000);
   };
 
-
-
   const handleClick = () => {
     if (isGameRunning) {
       setClickCount((prev) => prev + 1);
     }
   };
 
-  return (
-    <div>
+  const handleDurationChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const value = parseInt(e.target.value);
+    if (value >= 1) {
+      setDuration(value);
+    }
+  };
 
+  useEffect(() => {
+    return () => {
+      if (intervalRef.current) {
+        clearInterval(intervalRef.current);
+      }
+    };
+  }, []);
+
+
+  const clicksPerSecond = clickCount / duration;
+
+
+  return (
+    <div className="app-container">
+      <div className="game-card">
+        <h1>Click Speed Test</h1>
+
+        <div className='stats'>
+          <p>Time Left: {timeLeft}</p>
+          <p>Clicks: {clickCount}</p>
+          <p>High Score: {highScore}</p>
+          {!isGameRunning && timeLeft === 0 && (
+            <p>Clicks per Second: {clicksPerSecond.toFixed(2)}</p>
+          )}
+        </div>
+
+        <div className="duration-input">
+          <label> Test Duration (seconds): </label>
+          <input
+            type="number"
+            value={duration}
+            onChange={handleDurationChange}
+            disabled={isGameRunning}
+            min="1"
+          />
+
+          <div className='button-group'>
+            <button
+              onClick={startGame}
+              disabled={isGameRunning}
+              className={`start-button ${isGameRunning ? 'disabled' : ''}`}
+            >
+              Start Test
+            </button>
+
+            <button
+              onClick={handleClick}
+              disabled={!isGameRunning}
+              className={`click-button ${isGameRunning ? 'disabled' : ''}`}
+            >
+              Click Me!
+            </button>
+
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
